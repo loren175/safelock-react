27cc3b02d4bac75e206d425a6ab9c14df4ae4b06
Rafael Loren175 <rafael.loren175@gmail.com>
2024-01-05 14:31:34 -0300

    added aboutus content

---
+++ src/Components/AboutBG/AboutBG.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles"
+import about from "../../assets/about.jpg"
+
+export function AboutBG() {
+  return (
+    <Container>
+      <img src={about} alt="" />
+    </Container>
+  )
+}
+
+// Code BackGround from Services
--- src/pages/AboutUs/styles.js
+++ src/pages/AboutUs/styles.js
@@ -1,4 +1,17 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const Container = styled.div`
+  color: white;
 `
+
+export const Info = styled.div`
+  margin: 50px;
+  display: grid;
+  grid-template-columns: 200px 100px;
+  grid-template-rows: 400px 100px 100px;
+  align-items: center;
+  justify-content: center;
+
+`
+
+export const Contact = styled.div``
