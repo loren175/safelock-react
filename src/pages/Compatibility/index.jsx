4ac81bac4308bda50072c4bd1316e22625f68f10
Rafael Loren175 <rafael.loren175@gmail.com>
2024-01-10 15:46:49 -0300

    added structure to Compatibility page

---
+++ src/pages/Compatibility/index.jsx
@@ -0,0 +1,10 @@
+import Header from "../../Components/Header/Header"
+import { Container } from "./styles"
+
+export function Compatibility() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  )
+}
